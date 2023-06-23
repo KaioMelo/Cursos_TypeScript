9f430d4d05db6b45c83a57fc503a16747cced82d
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-06-22 21:31:05 -0300

    praticas de tipos em TypeScript

--- Tipos/tipos.js
+++ Tipos/tipos.js
@@ -1,6 +1,6 @@
 "use strict";
 //  já detecta o tipo da variavel na declaracao
-// String 
+// String
 let nome = "João";
 console.log(nome);
 // Numbers
@@ -9,13 +9,13 @@ console.log(idade);
 // Boolean
 let possuiHobbies = false;
 console.log(possuiHobbies);
-// Tipos explicitos 
+// Tipos explicitos
 // "any" pode ser Number, String e Boolean
 let minhaIdade;
 minhaIdade = 25;
 console.log(typeof minhaIdade);
-minhaIdade = "Idade é 27";
-console.log(typeof minhaIdade);
+// minhaIdade = "Idade é 27";
+// console.log(typeof minhaIdade);
 // Array
 let hobbies = ["Cozinhas", "Praticar"];
 console.log(hobbies);
@@ -27,4 +27,51 @@ console.log(typeof hobbies);
 let endereco = ["Central", 124];
 console.log(endereco);
 endereco = ["Manoel", 250];
-// Enums
+// Enums - valores pré definidos
+var Cor;
+(function (Cor) {
+    Cor[Cor["Cinza"] = 10] = "Cinza";
+    Cor[Cor["Verde"] = 11] = "Verde";
+    Cor[Cor["Azul"] = 100] = "Azul";
+    Cor[Cor["Vermelho"] = 101] = "Vermelho";
+    Cor[Cor["Amarelo"] = 102] = "Amarelo";
+})(Cor || (Cor = {}));
+let minhaCor = Cor.Amarelo;
+console.log("minhaCor");
+console.log(Cor.Azul);
+console.log(Cor.Cinza, Cor.Verde);
+console.log(Cor.Vermelho, Cor.Amarelo);
+// Any
+let carro = "BMW";
+console.log(carro);
+carro = { marca: "BMW", ano: 2019 };
+console.log(carro);
+// Funções 
+function retornaMeuNome() {
+    // return minhaIdade
+    return nome;
+}
+console.log(retornaMeuNome());
+function digaOI() {
+    console.log("OI....");
+}
+digaOI();
+function multiplicar(numA, numB) {
+    return numA * numB;
+}
+console.log(multiplicar(4, 9));
+// Tipo função
+let calculo;
+calculo = multiplicar;
+console.log(calculo(5, 6));
+//  Objetos
+let usuario = {
+    nome: "Joana",
+    idade: 24
+};
+console.log(usuario);
+usuario = {
+    idade: 25,
+    nome: "Kaio"
+};
+console.log(usuario);
--- Tipos/tipos.ts
+++ Tipos/tipos.ts
@@ -1,45 +1,108 @@
 //  já detecta o tipo da variavel na declaracao
 
-// String 
+// String
 
-let nome: string = "João"
-console.log(nome)
+let nome: string = "João";
+console.log(nome);
 
 // Numbers
 
-let idade: number = 27
-console.log(idade)
+let idade: number = 27;
+console.log(idade);
 
 // Boolean
 
-let possuiHobbies: boolean = false
-console.log(possuiHobbies)
+let possuiHobbies: boolean = false;
+console.log(possuiHobbies);
 
-// Tipos explicitos 
+// Tipos explicitos
 // "any" pode ser Number, String e Boolean
 
-let minhaIdade: any 
-minhaIdade = 25
-console.log(typeof minhaIdade)
-minhaIdade = "Idade é 27"
-console.log(typeof minhaIdade)
+let minhaIdade: number;
+minhaIdade = 25;
+console.log(typeof minhaIdade);
+// minhaIdade = "Idade é 27";
+// console.log(typeof minhaIdade);
 
 // Array
 
-let hobbies: any[] = ["Cozinhas", "Praticar"]
-console.log(hobbies)
-console.log(typeof hobbies)
+let hobbies: any[] = ["Cozinhas", "Praticar"];
+console.log(hobbies);
+console.log(typeof hobbies);
 
-hobbies = [100, 200, 300]
-console.log(hobbies)
-console.log(typeof hobbies)
+hobbies = [100, 200, 300];
+console.log(hobbies);
+console.log(typeof hobbies);
 
 //  Tuplas
 
-let endereco: [string, number] = ["Central", 124]
-console.log(endereco)
+let endereco: [string, number] = ["Central", 124];
+console.log(endereco);
 
-endereco = ["Manoel", 250]
+endereco = ["Manoel", 250];
 
-// Enums
+// Enums - valores pré definidos
 
+enum Cor {
+  Cinza = 10,
+  Verde,
+  Azul = 100,
+  Vermelho,
+  Amarelo,
+}
+
+let minhaCor: Cor = Cor.Amarelo;
+console.log("minhaCor");
+
+console.log(Cor.Azul)
+console.log(Cor.Cinza, Cor.Verde)
+console.log(Cor.Vermelho, Cor.Amarelo)
+
+// Any
+
+let carro: any = "BMW"
+console.log(carro)
+carro = {marca: "BMW", ano: 2019}
+console.log(carro)
+
+
+// Funções 
+
+function retornaMeuNome(): string{
+    // return minhaIdade
+    return nome
+}
+console.log(retornaMeuNome())
+
+function digaOI(): void{
+    console.log("OI....")
+}
+digaOI()
+
+function multiplicar(numA: number, numB: number): number{
+    return numA * numB
+}
+console.log(multiplicar(4, 9))
+
+// Tipo função
+
+let calculo: (x: number, y: number) => number
+
+calculo = multiplicar
+console.log(calculo(5, 6))
+
+//  Objetos
+
+let usuario: {nome: string, idade: number} = {
+    nome: "Joana",
+    idade: 24
+}
+
+console.log(usuario)
+
+usuario = {
+    idade: 25,
+    nome: "Kaio"
+}
+
+console.log(usuario)
